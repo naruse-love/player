df1a7572e441304841327331f08639a7b04b82c5
dj2733721464-cyber <dj2733721464@gmail.com>
2026-06-22 15:50:17 +0800

    chore: remove unused using System.Linq

--- .github/workflows/sfx_installer.cs
+++ .github/workflows/sfx_installer.cs
@@ -5,7 +5,6 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
-using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
